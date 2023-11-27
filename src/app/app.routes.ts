066cf6e18473947d32512575fb0bd0ce7b979f64
Stephan Grobler <stephan.grobler@gmail.com>
2023-11-27 10:09:24 +0200

    add localstorage timer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'timer',
     pathMatch: 'full',
   },
   {
     path: 'folder/:id',
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'timer',
+    loadComponent: () => import('./timer/timer.page').then( m => m.TimerPage)
   },
 ];
---
+++ src/app/timer/timer.page.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Observable, Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.page.html',
+  styleUrls: ['./timer.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
+})
+export class TimerPage implements OnInit {
+  start: Date | null = null;
+  end: Date | null = null;
+  elapsedTime: number = 0;
+  timeSubscription: Subscription | null = null;
+  isRunning = false;
+  sessions: [{ start: Date; end: Date; elapsedTime: number }?] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.retrieveSessions();
+    this.retrieveActiveSession();
+  }
+
+  onStart() {
+    if (!this.isRunning) {
+      this.start = new Date();
+      this.timeSubscription = interval(1000).subscribe((time) => {
+        if (this.start) {
+          this.elapsedTime = new Date().getTime() - this.start?.getTime();
+        }
+      });
+      this.isRunning = true;
+      this.persistActiveSession({start: this.start, end: null, elapsedTime: 0});
+    }
+  }
+
+  onEnd() {
+    if (this.isRunning) {
+      this.end = new Date();
+      if (this.start) {
+        this.sessions.push({
+          start: this.start,
+          end: this.end,
+          elapsedTime: this.elapsedTime,
+        });
+        this.persistSessions(this.sessions);
+      }
+      this.timeSubscription?.unsubscribe();
+      this.elapsedTime = 0;
+      this.isRunning = false;
+      localStorage.removeItem('active-fasting-session');
+    }
+  }
+
+  calculateDiff(start: Date, end: Date) {
+    return end.getUTCDate() - start.getUTCDate();
+  }
+
+  formatElapsedTime(elapsedTime: number): string {
+    const hours = Math.floor(elapsedTime / 3600000);
+    const minutes = Math.floor((elapsedTime % 3600000) / 60000);
+    const seconds = Math.floor((elapsedTime % 60000) / 1000);
+
+    const pad = (num: number): string => (num < 10 ? '0' : '') + num;
+
+    return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+  }
+
+  persistSessions(sessions: any){
+    localStorage.setItem('fasting-sessions', JSON.stringify(this.sessions));
+  }
+
+  retrieveSessions(){
+    if (localStorage.getItem('fasting-sessions')){
+      this.sessions = JSON.parse(localStorage.getItem('fasting-sessions')!);
+    } else {
+      localStorage.setItem('fasting-sessions', JSON.stringify([]));
+    }
+  }
+
+  persistActiveSession(session: any) {
+    localStorage.setItem('active-fasting-session', JSON.stringify(session));
+  }
+
+  retrieveActiveSession(){
+    if (localStorage.getItem('active-fasting-session')){
+      const session: {start: Date, end: Date, elapsedTime: number} = JSON.parse(localStorage.getItem('active-fasting-session')!);
+      console.log(session);
+      if (session?.start){
+        this.start = new Date(session.start);        
+        this.timeSubscription = interval(1000).subscribe((time) => {
+          if (this.start) {
+            this.elapsedTime = new Date().getTime() - this.start?.getTime();
+          }
+        });
+        this.isRunning = true;
+      }
+    }
+  }
+
+}
