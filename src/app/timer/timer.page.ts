edb040d830cb7b6ee5d0dc3990ae4d177d64fdb4
Stephan Grobler <stephan.grobler@gmail.com>
2023-11-27 11:04:07 +0200

    tweaks

--- src/app/timer/timer.page.ts
+++ src/app/timer/timer.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { Observable, Subscription, interval } from 'rxjs';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-timer',
